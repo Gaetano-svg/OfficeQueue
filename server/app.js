56551de82fb7430811d08dbe763d51c341cf8e08
Giosuè Valfrè <giosue.valfre@tecnologic-to.it>
2020-10-21 17:59:59 +0200

    fix bug API post

--- server/app.js
+++ server/app.js
@@ -10,16 +10,14 @@ const { fdatasync } = require('fs');
 
 var app = express();
 
-app.use('/', index(app));
 app.use(bodyParser.json({ limit: '50mb' }));
-//app.use(bodyParser.urlencoded({ extended: true, limit: '50mb', parameterLimit: 1000000 }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb', parameterLimit: 1000000 }));
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-
-
+app.use('/', index(app));
 
 app.io = require('./helpers/socketIo')();
 
